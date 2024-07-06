5639f0f9a2ec4579a251053f19de2240da1993da
Josh Peterson <joshuadanpeterson@gmail.com>
2024-07-06 04:10:17 -0600

    feat(typewriter_sounds): add additional Vim motions and modes mapping to typewriter sounds
    - Mapped additional Vim motions to typewriter sounds:
      - Motion Keys: gg, G, H, M, L, zz, zb, zt
      - Word Motions: ge, gE
      - Search Motions: /, ?, n, N, *, #
      - Text Objects: aw, iw, as, is, ap, ip, a[, i[, etc.
      - Miscellaneous: % (match parentheses)
    - Mapped additional Vim modes to typewriter sounds:
      - Visual Line Mode: V
      - Visual Block Mode: Ctrl-v
      - Command-line Mode: :
      - Replace Mode: R
      - Select Mode: gh, gH
      - Operator-pending Mode: d, y, c, >
    - Updated the script to capture global key events and determine the
    correct mode for sound playback.
    - Improved sound playback logic to handle the new motions and modes.

--- typewriter_sounds.py
+++ typewriter_sounds.py
@@ -6,48 +6,9 @@ from pynput import keyboard
 
 
 class TypeWriterSounds:
-    """
-    Typewriter sounds emulator for Python
-    =====================================
-
-    This program plays typewriter sounds each time a key is pressed, giving
-    the user the vintage experience of an old typewriter machine.
-
-    Sound samples come from https://www.freesound.org/, some were modified
-    for this project.
-
-    Requeriments
-    ------------
-
-    -  Python 3.x
-    -  `PyGame <http://pygame.org>`__ (for sound)
-    -  `pynput <https://pypi.org/project/pynput/>`__ (for global key capture)
-
-    Usage
-    -----
-
-    cd into the project's directory and type:
-
-    ::
-
-        $ python typewriter_sounds.py
-
-    to stop the program, just type CTRL-C.
-
-    Author
-    ------
-
-    Manuel Arturo Izquierdo aizquier@gmail.com
-
-    """
-
     def __init__(self):
-        # Initializes pygame mixer. A buffer of 512 bytes is required for better performance
         pygame.mixer.init(buffer=512)
-
         self.bellcount = 0
-
-        # Preloads sound samples
         self.keysounds = {
             "load": pygame.mixer.Sound("samples/manual_load_long.wav"),
             "shift": pygame.mixer.Sound("samples/manual_shift.wav"),
@@ -58,79 +19,173 @@ class TypeWriterSounds:
             "bell": pygame.mixer.Sound("samples/manual_bell.wav"),
         }
 
-        print("TypeWriter Sounds Emulator. v1.0")
-        print("type now and enjoy the vintage experience!...")
+    def play_sound(self, keyname, mode):
+        if mode in ("normal", "visual", "operator_pending"):
+            if keyname in (
+                "h",
+                "j",
+                "k",
+                "l",
+                "W",
+                "w",
+                "e",
+                "E",
+                "b",
+                "B",
+                "0",
+                "$",
+                "gg",
+                "G",
+                "H",
+                "M",
+                "L",
+                "zz",
+                "zb",
+                "zt",
+                "ge",
+                "gE",
+                "/",
+                "?",
+                "n",
+                "N",
+                "*",
+                "#",
+                "aw",
+                "iw",
+                "as",
+                "is",
+                "ap",
+                "ip",
+                "a[",
+                "i[",
+                "%",
+            ):
+                self.keysounds["shift"].play()
+            else:
+                self.play_default_sound(keyname)
+        elif mode == "insert":
+            self.play_default_sound(keyname)
 
-        self.run()
+    def play_default_sound(self, keyname):
+        if keyname in ("enter", "o", "O"):
+            self.keysounds["enter"].play()
+            self.bellcount = 0
+            self.keysounds["bell"].play()
+        elif keyname == "space":
+            self.keysounds["space"].play()
+            self.bellcount += 1
+        elif keyname in ("delete", "backspace"):
+            self.keysounds["delete"].play()
+            self.bellcount -= 1
+            if self.bellcount <= 0:
+                self.bellcount = 0
+        elif keyname in (
+            "up",
+            "down",
+            "left",
+            "right",
+            "ctrl_l",
+            "ctrl_r",
+            "shift_l",
+            "shift_r",
+            "alt_l",
+            "alt_r",
+            "tab",
+            "caps_lock",
+            "f1",
+            "f2",
+            "f3",
+            "f4",
+            "f5",
+            "f6",
+            "f7",
+            "f8",
+            "f9",
+            "f10",
+            "f11",
+            "f12",
+            "cmd",
+            "esc",
+        ):
+            self.keysounds["shift"].play()
+        elif keyname in ("page_up", "page_down", "home", "end"):
+            self.keysounds["load"].play()
+        else:
+            self.keysounds["key"].play()
+            self.bellcount += 1
 
-    def run(self):
+        if self.bellcount == 70:
+            self.keysounds["bell"].play()
+            self.bellcount = 0
+
+    def on_press(self, key):
+        try:
+            keyname = key.char if key.char else key.name
+
+            # Determine mode based on key pressed
+            if keyname in (
+                "h",
+                "j",
+                "k",
+                "l",
+                "W",
+                "w",
+                "e",
+                "E",
+                "b",
+                "B",
+                "0",
+                "$",
+                "gg",
+                "G",
+                "H",
+                "M",
+                "L",
+                "zz",
+                "zb",
+                "zt",
+                "ge",
+                "gE",
+                "/",
+                "?",
+                "n",
+                "N",
+                "*",
+                "#",
+                "aw",
+                "iw",
+                "as",
+                "is",
+                "ap",
+                "ip",
+                "a[",
+                "i[",
+                "%",
+            ):
+                mode = "normal"
+            elif keyname in ("V", "<C-v>"):
+                mode = "visual"
+            elif keyname in (":"):
+                mode = "command"
+            elif keyname in ("R"):
+                mode = "replace"
+            elif keyname in ("gh", "gH"):
+                mode = "select"
+            elif keyname in ("d", "y", "c", ">"):
+                mode = "operator_pending"
+            else:
+                mode = "insert"
+
+            self.play_sound(keyname, mode)
+        except AttributeError:
+            pass
+
+    def start_listener(self):
         listener = keyboard.Listener(on_press=self.on_press)
         listener.start()
         listener.join()
 
-    def on_press(self, key):
-        try:
-            if hasattr(key, "char") and key.char is not None:
-                keyname = key.char
-            else:
-                keyname = key.name
-
-            # Plays an audio sample according to the keyname
-            if keyname == "enter":
-                self.keysounds["enter"].play()
-                self.bellcount = 0  # Reset bellcount on new line
-                self.keysounds["bell"].play()  # Play bell sound on new line
-            elif keyname == "space":
-                self.keysounds["space"].play()
-                self.bellcount += 1
-            elif keyname in ("delete", "backspace"):
-                self.keysounds["delete"].play()
-                self.bellcount -= 1
-                if self.bellcount <= 0:
-                    self.bellcount = 0
-            elif keyname in (
-                "up",
-                "down",
-                "left",
-                "right",
-                "ctrl_l",
-                "ctrl_r",
-                "shift_l",
-                "shift_r",
-                "alt_l",
-                "alt_r",
-                "tab",
-                "caps_lock",
-                "f1",
-                "f2",
-                "f3",
-                "f4",
-                "f5",
-                "f6",
-                "f7",
-                "f8",
-                "f9",
-                "f10",
-                "f11",
-                "f12",
-                "cmd",
-                "esc",
-            ):
-                self.keysounds["shift"].play()
-            elif keyname in ("page_up", "page_down", "home", "end"):
-                self.keysounds["load"].play()
-            else:
-                self.keysounds["key"].play()
-                self.bellcount += 1
-
-            # After 70 consecutive keypresses, play the bell sound
-            if self.bellcount == 70:
-                self.keysounds["bell"].play()
-                self.bellcount = 0
-
-        except AttributeError:
-            pass
-
 
 if __name__ == "__main__":
-    TypeWriterSounds()
+    sounds = TypeWriterSounds()
+    sounds.start_listener()
